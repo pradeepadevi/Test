0389712c980befc47be076441085e50f9a837cca
vinuanbu19 <vinorika19@gmail>
2018-07-23 10:43:35 +0530

    XMLToJava Project

---
+++ pradeepa/XMLToJava/src/main/java/org/text/XMLToJava/App.java
@@ -0,0 +1,13 @@
+package org.text.XMLToJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
